ab41d0823e73d4b266b03b8877da1da6d45d25ae
Volodymyr Dzyura <103021185+volodymyrdzyura@users.noreply.github.com>
2022-09-12 16:44:55 +0300

    12.09.2022-4
    555

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,6 +18,7 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
+flatpickr("##datetime-picker", {});
 // ------------------------------------------------------------візуальне оформлення лічильника------------------------
 timer.style.display = 'flex';
 
